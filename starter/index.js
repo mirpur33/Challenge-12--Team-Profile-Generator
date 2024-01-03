6846fb50581c1992d2dde6d3ba0746fc58f19105
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2024-01-03 22:52:41 +0000

    index.js file code updating

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,37 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Array for answers to questions
+const newStaffMemberData = [];
+
+// Array object questions asked in CLI to user
+const questions = async () => {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What is your role?",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
+
+    //  tbc
+
+    
+} 
