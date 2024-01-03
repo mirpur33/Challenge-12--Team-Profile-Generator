25b8b0afecf86a54e0dbd95e2defd4bcbeb726cf
mirpur33 <146129859+mirpur33@users.noreply.github.com>
2024-01-03 02:36:25 +0000

    npm initiated and inquirer installed

---
+++ starter/lib/Employee.js
@@ -0,0 +1,3 @@
+// TODO: Write code to define and export the Employee class
+
+  
---
+++ starter/lib/Intern.js
@@ -0,0 +1,3 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+
